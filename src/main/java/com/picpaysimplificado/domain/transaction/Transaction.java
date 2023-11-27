ab279f80af3043114d133c4d49b9abfd361d60c8
judsonkerller <judsonkerller@gmail.com>
2023-11-27 09:27:06 -0300

    feat: create Transaction entity

---
+++ src/main/java/com/picpaysimplificado/domain/transaction/Transaction.java
@@ -0,0 +1,36 @@
+package com.picpaysimplificado.domain.transaction;
+
+import com.picpaysimplificado.domain.user.User;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity(name = "transcations")
+@Table(name = "transactions")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Transaction {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private BigDecimal balance;
+
+    @ManyToOne
+    @JoinColumn(name = "sender_id")
+    private User sender;
+
+    @ManyToOne
+    @JoinColumn(name = "receiver_id")
+    private User receiver;
+
+    private LocalDateTime timestamp;
+
+
+}
